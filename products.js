649edf5b6ed197ff82911c95f776447eddb160a1
akashg30 <akash.gudla@slu.edu>
2025-05-08 00:51:18 +0000

    lab 08

--- products.js
+++ products.js
@@ -1,6 +1,5 @@
 const cuid = require('cuid')
 const db = require('./db')
-
 // Define our Product Model
 const Product = db.model('Product', {
   _id: { type: String, default: cuid },
@@ -27,16 +26,13 @@ const Product = db.model('Product', {
     title: { type: String, required: true },
   }],
 })
-
 /**
  * List products
  * @param {*} options 
  * @returns 
  */
 async function list(options = {}) {
-
   const { offset = 0, limit = 25, tag } = options;
-
   const query = tag ? {
     tags: {
       $elemMatch: {
@@ -44,50 +40,45 @@ async function list(options = {}) {
       }
     }
   } : {}
-
+  
+  // Ensuring the function returns an array
   const products = await Product.find(query)
     .sort({ _id: 1 })
     .skip(offset)
     .limit(limit)
-
-  return products
-}
-
-/**
- * Get a single product
- * @param {string} id
- * @returns {Promise<object>}
- */
-async function get(_id) {
-  const product = await Product.findById(_id)
-  return product
-}
-
-async function create(fields) {
-  const product = await new Product(fields).save()
-  return product
-}
-
-async function edit(_id, change) {
-  const product = await get(_id)
-
-  Object.keys(change).forEach(function(key) {
-    product[key] = change[key]
-  })
-
-  await product.save()
-
-  return product
-}
-
-async function destroy(_id) {
-  return await Product.deleteOne({ _id })
-}
-
-module.exports = {
-  list,
-  create,
-  edit,
-  destroy,
-  get
-}
+    .exec(); // Using exec to ensure products are returned as an array
+    return products;  // This should return an array of products
+  }
+  
+  
+  /**
+   * Get a single product
+   * @param {string} id
+   * @returns {Promise<object>}
+   */
+  async function get(_id) {
+    const product = await Product.findById(_id)
+    return product
+  }
+  async function create(fields) {
+    const product = await new Product(fields).save()
+    return product
+  }
+  async function edit(_id, change) {
+    const product = await get(_id)
+    Object.keys(change).forEach(function(key) {
+      product[key] = change[key]
+    })
+    await product.save()
+    return product
+  }
+  async function destroy(_id) {
+    return await Product.deleteOne({ _id })
+  }
+  module.exports = {
+    list,
+    create,
+    edit,
+    destroy,
+    get
+  }
